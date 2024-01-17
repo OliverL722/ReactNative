2bcaee1bbc2eaacc48c1d3da2bb1f2dc5f87478f
OliverL722 <ololesiakowski@gmail.com>
2024-01-17 11:59:48 -0600

    New Updates

--- newApp/App.js
+++ newApp/App.js
@@ -1,25 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const {height, width} = Dimensions.get('window');
+  console.log("height" + height + " width " + width);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      paddingTop: 500,
+      //justifyContent: 'center',
+    },
+    textContainer: {
+      width: width*.8, //80% of screen
+      backgroundColor: "yellow",
+    },
+    text:{
+      fontSize: 40,
+      color: 'orange',
+    }
+  });
+
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <View style = {styles.textContainer}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <Text>It's Cold!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 500,
-    //justifyContent: 'center',
-  },
-  text:{
-    fontSize: 40,
-    color: 'red',
-  }
-});
